0a69a99540383d1669f036be6b6d93f35613eac8
Mark hanley <home@markhanley.co.uk>
2022-12-31 18:19:06 +0000

    Random colours and main objects added

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-let PALETTE_SIZE = 5;
+const palette = new Palette(document.querySelectorAll(".colour"), 5);
+const ADD_BUTTONS = document.querySelectorAll(".add-button");
+
+const GenerateMethod = {
+    Monochromatic: 0,
+    Analogous: 1,
+    Complementary: 2,
+    Random: 3,
+};
 
 function Colour(red, green, blue) {
     this.red = red;
@@ -21,6 +29,47 @@ function Colour(red, green, blue) {
     }
 };
 
-col = new Colour(1, 254, 253);
+function Palette(paletteElements, size = 5, method) {
+    this.paletteElements = paletteElements;
+    // Generate colours array with 5 null items so that it can be iterated over
+    this.paletteColours = Array.apply(null, Array(5)).map(function () { });
 
-console.log(col.getHexString());
+    this.generate = function (method) {
+        switch (method) {
+            case GenerateMethod.Random:
+                this.generateRandom();
+                break;
+            default:
+                this.generateRandom();
+        }
+    };
+
+    this.generateRandom = function () {
+        for (let i = 0; i < this.paletteColours.length; i++) {
+            this.paletteColours[i] = new Colour(
+                Math.floor(Math.random() * 256),
+                Math.floor(Math.random() * 256),
+                Math.floor(Math.random() * 256));
+        };
+
+        console.log(`Palette: ${this.paletteColours}`);
+    }
+
+    this.display = function () {
+        numberOfColours = this.paletteColours.length;
+        this.paletteElements.forEach((element, index) => {
+            if (index < numberOfColours) {
+                element.style.backgroundColor = this.paletteColours[index].getHexString();
+                element.classList.remove("hidden");
+                element.classList.add("visible");
+            }
+            else {
+                element.classList.remove("visible");
+                element.classList.add("hidden");
+            }
+        });
+    }
+}
+
+palette.generate(GenerateMethod.Random);
+palette.display();
